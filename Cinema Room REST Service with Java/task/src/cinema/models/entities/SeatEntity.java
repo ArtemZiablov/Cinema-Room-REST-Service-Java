e00c5b3fb17cdd6bcad0112f5ce3c741edd7b053
Artem_Ziablov <artem.ziablov13@gmail.com>
2024-05-15 21:14:14 +0200

    tested h2-file db

--- Cinema Room REST Service with Java/task/src/cinema/models/entities/SeatEntity.java
+++ Cinema Room REST Service with Java/task/src/cinema/models/entities/SeatEntity.java
@@ -2,15 +2,20 @@ package cinema.models.entities;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.*;
 
 @Builder
-@NoArgsConstructor@AllArgsConstructor
-@Getter@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @ToString
 @Entity
-@Table(name = "seats")
+@Table(name = "seats", indexes = {
+        @Index(name = "idx_seat_token", columnList = "token")
+})
 public class SeatEntity {
     @EmbeddedId
     SeatCoordinatesId coordinates;
